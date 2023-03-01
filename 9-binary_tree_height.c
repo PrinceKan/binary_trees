7417007cfccf22702f5ca4acf2a875430a64beef
PrinceKan <princekan9.mk@gmail.com>
2023-03-01 16:39:24 +0000

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,21 +11,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t right_knot, left_knot;
 
-	if (tree != NULL)
-	{
-		if ((tree->left == NULL) && (tree->right == NULL))
-			return (0);
+	if (tree == NULL)
+		return (0);
 
-		left_knot = binary_tree_height(tree->left);
-		right_knot = binary_tree_height(tree->right);
-		if (right_knot < left_knot)
-		{
-			return (right_knot + 1);
-		}
-		else
-		{
-			return (left_knot + 1);
-		}
-	}
-	return (0);
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (0);
+
+	left_knot = binary_tree_height(tree->left);
+	right_knot = binary_tree_height(tree->right);
+
+	if (right_knot < left_knot)
+		return (right_knot + 1);
+	else
+		return (left_knot + 1);
 }
