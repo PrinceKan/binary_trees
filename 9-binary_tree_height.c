e15e3580ad3260b3357283006f67db118b81479a
PrinceKan <princekan9.mk@gmail.com>
2023-03-02 00:36:54 +0000

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -20,7 +20,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_knot = binary_tree_height(tree->left);
 	right_knot = binary_tree_height(tree->right);
 
-	if (right_knot < left_knot)
+	if (right_knot > left_knot)
 		return (right_knot + 1);
 	else
 		return (left_knot + 1);
